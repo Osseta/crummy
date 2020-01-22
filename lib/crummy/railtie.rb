9d73c066f91bdcdd8640314c30172d096882b10f
Tomas Varneckas <tomas@varneckas.lt>
2020-01-22 16:14:08 +0200

    Fix Railtie initialization process to work well with Rails 6 autoloader

--- lib/crummy/railtie.rb
+++ lib/crummy/railtie.rb
@@ -4,15 +4,17 @@ require 'rails'
 module Crummy
   class Railtie < Rails::Railtie
     initializer "crummy.action_controller" do |app|
-      if defined?(ActionController)
+      ActiveSupport.on_load :action_controller do
         require 'crummy/action_controller'
-	ActionController::Base.send :include,  Crummy::ControllerMethods 
+	include Crummy::ControllerMethods
       end
     end
-    
+
     initializer "crummy.action_view" do |app|
-      require 'crummy/action_view'
-      ActionView::Base.send :include, Crummy::ViewMethods
+      ActiveSupport.on_load :action_view do
+        require 'crummy/action_view'
+        include Crummy::ViewMethods
+      end
     end
   end
 end
